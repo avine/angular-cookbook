c975d48d0124bb01f469373157393647ab2302b4
Stéphane Francel <contact@avine.io>
2018-02-16 00:36:46 +0100

    Improve paths in create-js

--- create-app.js
+++ create-app.js
@@ -1,46 +1,48 @@
 #! /usr/bin/env node
 
 const fs = require('fs-extra');
+const path = require('path');
 
 const DEMO_MODE = false;
+const APPS_DIR = 'cookbook';
 
-const BASE_DIR = 'cookbook'; // Without "slash" at the end
-
-// Invoke this script using `node create-app [APP_NAME]` (same as invoking directly `./create-app.js [APP_NAME]`)
+// Get app name from command line argument (`node create-app [APP_NAME]`)
 let appName = process.argv[2];
 
-// argument mandatory
+// App name mandatory
 appName || exit('argument appName is missing.');
 
 appName = appName.toLowerCase();
 
-const appsList = fs.readdirSync(BASE_DIR);
+const appsList = fs.readdirSync(getPath(APPS_DIR));
 
-// template app mandatory
+// Template app mandatory
 appsList.includes('_tmpl') || exit('template app "_tmpl" is missing.');
 
-// new argument mandatory
+// New app name mandatory
 !appsList.includes(appName) || exit(`app "${appName}" already exists.`);
 
+const appRoot = `${APPS_DIR}/${appName}`;
+
 try {
-  DEMO_MODE || fs.copySync(`${BASE_DIR}/_tmpl`, `${BASE_DIR}/${appName}`);
-  console.log(`Created folder: ${BASE_DIR}/${appName}`);
+  DEMO_MODE || fs.copySync(getPath(`${APPS_DIR}/_tmpl`), getPath(appRoot));
+  console.log(`Created folder: ${appRoot}`);
 } catch (e) {
   exit(e.message);
 }
 
 try {
   // Get "_tmpl" app configuration
-  const config = JSON.parse(fs.readFileSync('.angular-cli.json', 'utf8'));
+  const config = JSON.parse(fs.readFileSync(getPath('.angular-cli.json'), 'utf8'));
   const tmplConfig = config.apps.filter(app => app.name === '_tmpl')[0];
 
-  // Clean configuration (when `app.name` has been removed from `BASE_DIR` folder)
+  // Clean configuration (when `app.name` has been removed from `APPS_DIR` folder)
   config.apps = config.apps.filter(app => app.root === 'src' || appsList.includes(app.name));
 
   // Set appName configuration
   const appConfig = Object.assign({}, tmplConfig);
   appConfig.name = appName;
-  appConfig.root = `${BASE_DIR}/${appName}`;
+  appConfig.root = appRoot;
 
   // Add appName configuration
   config.apps.push(appConfig);
@@ -50,7 +52,9 @@ try {
 
   // Update configuration
   const configString = JSON.stringify(config, undefined, 2);
-  DEMO_MODE ? console.log(`\n${configString}\n`) : fs.writeFileSync('.angular-cli.json', configString, 'utf8');
+  DEMO_MODE
+    ? console.log(`\n${configString}\n`)
+    : fs.writeFileSync(getPath('.angular-cli.json'), configString, 'utf8');
 
   console.log('Updated file: .angular-cli.json');
 } catch (e) {
@@ -59,8 +63,13 @@ try {
 
 console.log(DEMO_MODE ? '** IN DEMO_MODE (nothing done) **' : 'Operation completed!');
 
-// Helper
+// Helpers
+
 function exit(msg) {
   console.error(`Error: ${msg}`);
   process.exit(1);
+}
+
+function getPath(relativePath) {
+  return path.resolve(__dirname, relativePath)
 }
